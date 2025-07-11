b0cbdd0c5acf7bad0871ed59a18c78cde798c032
AleksGit123 <aleksichokheli03@gmail.com>
2025-07-11 16:42:59 +0400

    header finished

--- script.js
+++ script.js
@@ -3,3 +3,26 @@
 //   .then(data => console.log(data))
   
 
+let loginDiv = document.querySelector(".log_in_div");
+let userSvg = document.querySelector(".svg");
+let userGif = document.querySelector(".gif");
+let carGif = document.querySelector(".minicar_gif");
+let locationIcon = document.querySelector(".fa-location-dot");
+loginDiv.addEventListener("mouseenter",()=>{
+   userSvg.classList.add("hide");  
+   userGif.classList.remove("hide");
+})
+
+loginDiv.addEventListener("mouseleave",()=>{
+    userSvg.classList.remove("hide");  
+    userGif.classList.add("hide");
+})
+
+locationIcon.addEventListener("mouseover",()=>{
+    carGif.classList.remove("hide");
+})
+
+locationIcon.addEventListener("mouseout",()=>{
+    carGif.classList.add("hide");
+})
+
